814df2c3d71765bb8651d74d5504ae14b0b65534
Aditya Patra <adityabpatra@gmail.com>
2025-11-29 15:53:59 -0800

    add record function

--- main.cpp
+++ main.cpp
@@ -15,8 +15,14 @@ int main() {
     int input = -1;
     while (input != 5) {
         cout << "Menu: \n  1 - Add Record\n  2 - Delete Record\n  3 - Search Record\n  4 - Modify Records\n  5 - Exit" << endl;
-        cout << "Enter your choice: ";
+        cout << "Enter your choice:  ";
         cin >> input;
+        if (input == 1) {
+            string valToAdd;
+            cout << "Enter value to add: ";
+            cin >> valToAdd;
+            tree.insertNode(valToAdd);
+        }
     }
     return 0;
 }
