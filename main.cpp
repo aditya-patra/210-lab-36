d5f963410c7c0216cde0ff0b50aa324ca3202e3c
Aditya Patra <adityabpatra@gmail.com>
2025-11-29 15:09:37 -0800

    testing connection to file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <fstream>
 #include "IntBinaryTree.h"
 using namespace std;
 
 int main() {
-    
+    ifstream file("codes.txt");
+    string code;
+    while(getline(file, code)) {
+        cout << code << endl;
+    }
 
     return 0;
 }
