60a64e5aad359b5a06588165ed6a8800db6b912d
Aditya Patra <adityabpatra@gmail.com>
2025-11-29 15:34:49 -0800

    Milestone 2

--- main.cpp
+++ main.cpp
@@ -6,13 +6,15 @@ using namespace std;
 int main() {
     ifstream file("codes.txt");
     string code;
-    while(getline(file,code)) {
+    while(getline(file,code)) { 
         cout << code << endl;
         break;
     }
     IntBinaryTree tree;
-    string insertVal = "blue";
-    tree.insertNode(insertVal);
+    tree.insertNode("blue");
+    tree.insertNode("green");
+    tree.insertNode("purple");
+    tree.insertNode("black");
     tree.displayInOrder();
 
     return 0;
