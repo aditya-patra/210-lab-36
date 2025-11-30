fe1c5b0dd7ead02ffb73e2de04abaf853dd26537
Aditya Patra <adityabpatra@gmail.com>
2025-11-29 16:12:26 -0800

    comment

--- main.cpp
+++ main.cpp
@@ -8,32 +8,34 @@ int main() {
     string code;
     IntBinaryTree tree;
     cout << "Creating binary search tree" << endl << endl;
-    int f = 0;
+    // create BST
     while(getline(file,code)) { 
         tree.insertNode(code);
-        f++;
-        if (f == 10) {
-            break;
-        }
     }
+    // menu function
     int input = -1;
     while (input != 5) {
+        // display BST
         tree.displayInOrder();
+        // menu display
         cout << "\nMenu: \n  1 - Add Record\n  2 - Delete Record\n  3 - Search Record\n  4 - Modify Records\n  5 - Exit" << endl;
         cout << "Enter your choice:  ";
         cin >> input;
+        // add record
         if (input == 1) {
             string valToAdd;
             cout << "Enter value to add: ";
             cin >> valToAdd;
             tree.insertNode(valToAdd);
         }
+        // remove record
         else if (input == 2) {
             string valToDelete;
             cout << "Enter value to delete: ";
             cin >> valToDelete;
             tree.remove(valToDelete);
         }
+        // search for record
         else if (input == 3) {
             string valToSearch;
             cout << "Enter value to search for: ";
@@ -46,6 +48,7 @@ int main() {
                 cout << "Record does not exist within BST" << endl;
             }
         }
+        // modify record(add and delete)
         else if (input == 4) {
             string valToDelete;
             cout << "Enter value to modify: ";
